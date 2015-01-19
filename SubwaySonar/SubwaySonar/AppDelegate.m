4cc2c6d64c164ec726cecca6992c20fbf51bc7f2
Aaron Vasquez <aaronvasquez1@gmail.com>
2015-01-18 20:13:36 -0500

    removes storyboard

--- SubwaySonar/SubwaySonar/AppDelegate.m
+++ SubwaySonar/SubwaySonar/AppDelegate.m
@@ -6,10 +6,22 @@
 
 @implementation AppDelegate
 
+- (UIWindow *)window {
+  if (!_window) {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+  }
+  return _window;
+}
+
+#pragma mark - Protocols
+#pragma mark UIApplicationDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+  UIViewController *tmpViewController = [[UIViewController alloc] init];
+  tmpViewController.view.backgroundColor = [UIColor greenColor];
+  self.window.rootViewController = tmpViewController;
   return YES;
 }
 
