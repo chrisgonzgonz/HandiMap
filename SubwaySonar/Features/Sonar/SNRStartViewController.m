8453adb383c04dc24b3154794c6fa646b430b141
Aaron Vasquez <aaronvasquez1@gmail.com>
2015-01-21 21:43:32 -0500

    binds button to UIViewController

--- SubwaySonar/Features/Sonar/SNRStartViewController.m
+++ SubwaySonar/Features/Sonar/SNRStartViewController.m
@@ -2,12 +2,35 @@
 
 #import "SNRStartView.h"
 
+@interface SNRStartViewController()
+@property(nonatomic) SNRStartView *view;
+@end
+
 @implementation SNRStartViewController
 
 #pragma mark - Overrides
 
 - (void)loadView {
   self.view = [[SNRStartView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  [self bindViews];
+}
+
+#pragma mark - Target Actions
+
+- (void)startSonar:(UIButton *)sender {
+  NSLog(@"butter.");
+}
+
+#pragma mark - Private
+
+- (void)bindViews {
+  [self.view.sonarStartButton addTarget:self
+                                 action:@selector(startSonar:)
+                       forControlEvents:UIControlEventTouchUpInside];
 }
 
 @end
