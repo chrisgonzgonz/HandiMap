8b89bfcb3a36a29326d28ca38c50fa68a07e0b4e
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-01-24 16:18:31 -0500

    creates HNDSubwayMapView

---
+++ HandiMap/Features/Sonar/SubwayMap/HNDSubwayMapView.h
@@ -0,0 +1,13 @@
+//
+//  HNDSubwayMapView.h
+//  HandiMap
+//
+//  Created by Chris Gonzales on 1/24/15.
+//  Copyright (c) 2015 FSDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNDSubwayMapView : UIView
+
+@end
---
+++ HandiMap/Features/Sonar/SubwayMap/HNDSubwayMapView.m
@@ -0,0 +1,21 @@
+//
+//  HNDSubwayMapView.m
+//  HandiMap
+//
+//  Created by Chris Gonzales on 1/24/15.
+//  Copyright (c) 2015 FSDC. All rights reserved.
+//
+
+#import "HNDSubwayMapView.h"
+
+@implementation HNDSubwayMapView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
