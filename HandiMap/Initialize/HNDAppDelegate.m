8bd82761916a1aef672cf1c78a4ac87073fa46cd
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-01-24 16:13:44 -0500

    removes Flow property from HNDAppDelegate

--- HandiMap/Initialize/HNDAppDelegate.m
+++ HandiMap/Initialize/HNDAppDelegate.m
@@ -2,14 +2,6 @@
 
 #import "HNDSonarFlow.h"
 
-@interface HNDAppDelegate()
-
-// Putting this here for now so that it does not get released. Is there a better way?
-// TODO: Create an HNDFlow base class to remove this dependency.
-@property(nonatomic) HNDSonarFlow *rootFlow;
-
-@end
-
 @implementation HNDAppDelegate
 
 - (UIWindow *)window {
@@ -20,19 +12,12 @@
   return _window;
 }
 
-- (HNDSonarFlow *)rootFlow {
-  if (!_rootFlow) {
-    _rootFlow = [[HNDSonarFlow alloc] init];
-  }
-  return _rootFlow;
-}
-
 #pragma mark - Protocols
 #pragma mark UIApplicationDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  self.window.rootViewController = [self.rootFlow initialViewController];
+  self.window.rootViewController = [[[HNDSonarFlow alloc] init] initialViewController];
   return YES;
 }
 
