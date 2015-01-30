be628a37d7ba129221673ce593b0ffd4bef59ea3
Aaron Vasquez <aaronvasquez1@gmail.com>
2015-01-29 21:09:58 -0500

    documents null subwayLine

--- HandiMap/HNDSubwayLinesList.h
+++ HandiMap/HNDSubwayLinesList.h
@@ -1,5 +1,8 @@
 #import <Foundation/Foundation.h>
 
 @interface HNDSubwayLinesList : NSObject
+
+/// The last line returned will represent no filter.
 @property(nonatomic, readonly) NSArray *lines;
+
 @end
