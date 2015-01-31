9f8f35f5f719cbd18a0f03658ddc4f30a92fe4ba
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-01-31 00:20:41 -0500

    adds HNDStationDetailViewController & HNDStationDetailView

---
+++ HandiMap/Features/SubwayMap/HNDStationDetailView.h
@@ -0,0 +1,14 @@
+//
+//  HNDStationDetailView.h
+//  HandiMap
+//
+//  Created by Chris Gonzales on 1/30/15.
+//  Copyright (c) 2015 FSDC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNDStationDetailView : UIView
+@property (weak, nonatomic, readonly) UITableView *tableView;
+@property (weak, nonatomic, readonly) UIButton *outtageButton;
+@end
