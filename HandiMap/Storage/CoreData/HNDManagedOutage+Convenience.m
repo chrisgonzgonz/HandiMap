f56ad950389ae83967c32ec08badad7eeba8171f
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-01-31 20:18:34 -0500

    unfinished, tableview autolayout

--- HandiMap/Storage/CoreData/HNDManagedOutage+Convenience.m
+++ HandiMap/Storage/CoreData/HNDManagedOutage+Convenience.m
@@ -1,5 +1,4 @@
 #import "HNDManagedOutage+Convenience.h"
-
 @implementation HNDManagedOutage (Convenience)
 
 - (instancetype)initWithEntity:(NSEntityDescription *)entity
@@ -16,8 +15,12 @@
   self.equipmentType = dictionary[@"equipment_type"];
   self.routesAffected = dictionary[@"routes_affected"];
   self.reason = dictionary[@"reason"];
-  self.outageStartDate = dictionary[@"outage_start_date"];
-  self.estimatedReturnOfService = dictionary[@"estimated_return_of_service"];
+  
+  double outageDate = [dictionary[@"outage_start_date"] doubleValue];
+  self.outageStartDate = [NSDate dateWithTimeIntervalSince1970:outageDate];
+  double estimatedReturnOfService = [dictionary[@"estimated_return_of_service"] doubleValue];
+  self.estimatedReturnOfService = [NSDate dateWithTimeIntervalSince1970:estimatedReturnOfService];
+  
   self.ada = [NSNumber numberWithBool:[dictionary[@"ada"] boolValue]];
   self.updatedAt = [NSDate date];
 }
