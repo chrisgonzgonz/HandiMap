d3d4477d6482257c3a014181be99411157dc09ca
Aaron Vasquez <aaronvasquez1@gmail.com>
2015-02-01 16:25:33 -0500

    starts hockey manager before loading data

--- HandiMap/Initialize/HNDAppDelegate.m
+++ HandiMap/Initialize/HNDAppDelegate.m
@@ -1,12 +1,16 @@
 #import "HNDAppDelegate.h"
 
+#import <HockeySDK.h>
+
 #import "HNDDataStore.h"
 #import "HNDMapFlow.h"
-#import <HockeySDK.h>
+
+static NSString *const kHockeyId = @"f7b9bbd12bb0d75390f8dd8894f8f70a";
 
 @interface HNDAppDelegate()
 @property(nonatomic) HNDMapFlow *currentFlow;
 @end
+
 @implementation HNDAppDelegate
 
 - (UIWindow *)window {
@@ -29,32 +33,30 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [self startHockey];
   [self preloadData];
-
-  [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+  [self setupDefaults];
   self.window.rootViewController = [self.currentFlow initialViewController];
-  
-  [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:@"f7b9bbd12bb0d75390f8dd8894f8f70a"];
-  [[BITHockeyManager sharedHockeyManager] startManager];
-  [[BITHockeyManager sharedHockeyManager].authenticator
-   
-   authenticateInstallation];
-
   return YES;
 }
 
 #pragma mark - Private
 
+- (void)setupDefaults {
+  [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+}
+
 - (void)preloadData {
   [[HNDDataStore sharedStore] loadStationsWithSuccess:^{
     [[HNDDataStore sharedStore] loadOutagesWithSuccess:^{
+    } failure:^{ NSLog(@"Failed to load outages fuck me"); }];
+  } failure:^{ NSLog(@"Failed to load stations fuck me"); }];
+}
 
-    } failure:^{
-      NSLog(@"Failed to load outages fuck me");
-    }];
-  } failure:^{
-    NSLog(@"Failed to load stations fuck me");
-  }];
+- (void)startHockey {
+  [[BITHockeyManager sharedHockeyManager] configureWithIdentifier:kHockeyId];
+  [[BITHockeyManager sharedHockeyManager] startManager];
+  [[BITHockeyManager sharedHockeyManager].authenticator authenticateInstallation];
 }
 
 @end
