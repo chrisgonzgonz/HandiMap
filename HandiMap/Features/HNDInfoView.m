48eb52ebddd5a3f055aa15d9bc01c7ed057aff4e
Chris Gonzales <gonzales.chris.a@gmail.com>
2015-02-03 14:38:48 -0500

    Revert "fixes pin color bug from previous commit"
    This reverts commit f3381409f8f48718707403747d013018d09dbaeb.

--- HandiMap/Features/HNDInfoView.m
+++ HandiMap/Features/HNDInfoView.m
@@ -28,12 +28,9 @@
     for (ZSPinAnnotation *pin in @[_normalPin, _warningPin, _errorPin]) {
       pin = [[ZSPinAnnotation alloc] initWithAnnotation:nil reuseIdentifier:nil];
       pin.annotationType = ZSPinAnnotationTypeTag;
+      pin.annotationColor = [HNDColor highlightColor];
       pin.contentMode = UIViewContentModeScaleAspectFit;  
     }
-    _normalPin.annotationColor = [HNDColor highlightColor];
-    _warningPin.annotationColor = [HNDColor warningColor];
-    _errorPin.annotationColor = [HNDColor errorColor];
-    
     
     for (UIlabel *description in @[_normalDescription, _warningDescription, _errorDescription]) {
       description = [[[HNDLabel alloc] init] alignCenter];
